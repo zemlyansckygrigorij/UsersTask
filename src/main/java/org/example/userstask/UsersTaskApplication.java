efa97581bab922fd2498c06379e772e1d6cfdb76
zemlyansckyG <grigory.zemlyansky@ecm-consulting.ru>
2024-10-15 13:17:28 +0500

    Initial commit

---
+++ src/main/java/org/example/userstask/UsersTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.userstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersTaskApplication.class, args);
+	}
+
+}
